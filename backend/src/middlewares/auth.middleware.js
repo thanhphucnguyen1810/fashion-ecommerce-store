cf996ebfbf5de69048fe616b22fa205621c43359
thanhphucnguyen1810 <thanhphucnguyen54@gmail.com>
2025-06-25 16:21:49 +0700

    feat: Add Product routes api

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -23,6 +23,16 @@ const protect = async (req, res, next) => {
   }
 }
 
+// Middleware to check if the user is an admin
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next()
+  } else {
+    res.status(403).json({ message: 'Not authorized as an admin' })
+  }
+}
+
 export {
-  protect
+  protect,
+  admin
 }
